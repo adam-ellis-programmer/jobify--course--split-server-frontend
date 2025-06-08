fe74cd60ad8e00913cdc06a1a14626007b30cceb
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-08 11:40:35 +0100

    server cors update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import cookieParser from 'cookie-parser'
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'],
+    origin: ['http://localhost:5173', 'https://jobify-frontend-olive.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow cookies/auth headers
