d6e9f91e82cce81ac001b4290235ab83cef39f5c
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-08 11:47:01 +0100

    server cors update

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
+import axios from 'axios'
+
 const customFetch = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://jobify-course-backend-server.onrender.com/api/v1' // Your Render URL
-      : '/api/v1', // Keep proxy for local development
+      ? 'https://jobify-course-backend-server.onrender.com/api/v1'
+      : '/api/v1',
+  withCredentials: true, // ADD THIS LINE
 })
 
 export default customFetch
