c5a423251d49b2f61aab18fd1981f7671c7513f4
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-08 11:33:25 +0100

    custom fetch frontend change

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const customFetch = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://your-backend-name.onrender.com/api/v1' // Your Render URL
+      ? 'https://jobify-course-backend-server.onrender.com/api/v1' // Your Render URL
       : '/api/v1', // Keep proxy for local development
 })
 
