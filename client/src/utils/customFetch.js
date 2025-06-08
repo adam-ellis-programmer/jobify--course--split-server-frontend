fa85cf61967c1d759e3969fa24190c99779bb581
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-08 14:37:10 +0100

    server test

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,12 +1,8 @@
-import axios from 'axios'
-import axios from 'axios'
+import axios from 'axios' // Remove duplicate
 
 const customFetch = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? 'https://jobify-course-backend-server.onrender.com/api/v1'
-      : '/api/v1',
-  withCredentials: true, // ADD THIS LINE
+  baseURL: 'https://jobify-course-backend-server.onrender.com/api/v1', // Hardcode for now
+  withCredentials: true,
 })
 
 export default customFetch
--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ export const register = async (req, res) => {
   const user = await User.create(req.body)
   res.status(StatusCodes.CREATED).json({ msg: 'user created!' })
 }
-
+// ...
 export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email })
 
