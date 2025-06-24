db3195933542a2329755c31b0b78a56b15596a58
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-24 19:49:23 +0100

    new railway server url

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,9 +1,16 @@
 import axios from 'axios'
 
+// const customFetch = axios.create({
+//   baseURL:
+//     process.env.NODE_ENV === 'production'
+//       ? 'https://jobify-course-backend-server.onrender.com/api/v1'
+//       : '/api/v1', // Uses Vite proxy for local development
+//   withCredentials: true,
+// })
 const customFetch = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://jobify-course-backend-server.onrender.com/api/v1'
+      ? 'https://jobify-course-split-server-frontend-production.up.railway.app/api/v1'
       : '/api/v1', // Uses Vite proxy for local development
   withCredentials: true,
 })
